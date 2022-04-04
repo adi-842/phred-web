f62eeac09cc8ce9fab2b34abbe4d02174bb6fe1e
Aditya Raj <aditya.raj@duke.edu>
2022-04-04 12:58:12 -0400

    v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Phred Webpage</h1>
+      <h2>trial github page</h2>
+    </div>
+  );
+}
+
+export default App;
